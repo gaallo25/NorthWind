70db435c0b7670b48a5bee3f11742a065fb2b40c
Carlos Gallo <gaalloo_25c@hotmail.com>
2024-02-07 17:59:13 -0500

    Repositories

--- NorthWind.Sales.Backend.BusinessObjects/obj/Debug/net8.0/NorthWind.Sales.Backend.BusinessObjects.AssemblyInfo.cs
+++ NorthWind.Sales.Backend.BusinessObjects/obj/Debug/net8.0/NorthWind.Sales.Backend.BusinessObjects.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NorthWind.Sales.Backend.BusinessObjects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faed95a6effeb3988c0d8239fa99224be6ad35b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a230d67673aedd5b6b398dea14888a84fab26ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("NorthWind.Sales.Backend.BusinessObjects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NorthWind.Sales.Backend.BusinessObjects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
